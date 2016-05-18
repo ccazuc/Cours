7bdae1d41469130b4ff49f409d02a4bf7d99882e
Clement Cazuc <cc140988@localhost.univlehavre.lan>
2016-05-18 09:06:31 +0200

    exam

--- src/exam/Rationnel.java
+++ src/exam/Rationnel.java
@@ -8,19 +8,22 @@ public class Rationnel {
 	
 	/**
 	 * Create a new <code>Rationnel</code> with two given integer
+	 * @param numerateur
+	 * @param denominateur
 	 */
-	public Rationnel(int numerateur, int denominateur) { // Le denominateur doit être différent de 
+	public Rationnel(int numerateur, int denominateur) { // Le denominateur doit être différent de 0
 		this.numerateur = numerateur;
 		if(denominateur != 0) {
 			this.denominateur = denominateur;
 		}
 		else {
-			System.out.println("Error, le dénominateur doit être différent de 0");
+			System.out.println("Denominateur has to be different of 0");
 		}
 	}
 	
 	/**
 	 * Create a new integer
+	 * @param numerateur
 	 */
 	public Rationnel(int numerateur) {
 		this.numerateur = numerateur;
@@ -28,11 +31,12 @@ public class Rationnel {
 	}
 	
 	/**
-	 * Méthode alternative qui stock le numerateur et denominateur dans un tableau d'int
+	 * Alternativ method which store numerator and denominator into an array
+	 * @param number
 	 */
 	public Rationnel(int[] number) {
 		if(number.length != 2) {
-			System.out.println("Le tableau doit être de taille 2");
+			System.out.println("Array size must be equal to 2");
 		}
 		else {
 			this.number = number;
@@ -41,6 +45,7 @@ public class Rationnel {
 	
 	/**
 	 * return the numerateur of a <code>Rationnel</code>
+	 * @return <code>int</code>
 	 */
 	public int getNumerateur() {
 		return numerateur;
@@ -48,13 +53,15 @@ public class Rationnel {
 	
 	/**
 	 * set the numerateur with a given integer
+	 * @param numerateur
 	 */
 	public void setNumerateur(int numerateur) {
 		this.numerateur = numerateur;
 	}
 	
 	/**
-	 * return the denominateur of a <code>Rationnel</code>
+	 * returns the denominateur of a <code>Rationnel</code>
+	 * @return <code>int</code>
 	 */
 	public int getDenominateur() {
 		return this.denominateur;
@@ -62,18 +69,19 @@ public class Rationnel {
 	
 	/**
 	 * set the denominateur with a given integer
+	 * @param denominateur
 	 */
 	public void setDenominateur(int denominateur) {
 		if(denominateur != 0) {
 			this.denominateur = denominateur;
 		}
 		else {
-			System.out.println("Le dénominateur doit être différent de 0");
+			System.out.println("Denominator has to be different of 0");
 		}
 	}
 	
 	/**
-	 * return a String with a numerateur/denominateur format
+	 * returns a String with a numerateur/denominateur format
 	 */
 	public String toString() {
 		return this.numerateur+"/"+this.denominateur;
@@ -81,6 +89,8 @@ public class Rationnel {
 	
 	/**
 	 * add one existing <code>Rationnel</code> to a given <code>Rationnel</code>
+	 * @param number
+	 * @return <code>Rationnel</code>
 	 */
 	public Rationnel add(Rationnel number) {
 		return new Rationnel(this.numerateur*number.getDenominateur()+this.denominateur*number.getNumerateur(), this.denominateur*number.getDenominateur());
@@ -88,13 +98,16 @@ public class Rationnel {
 	
 	/**
 	 * multiply an existing <code>Rationnel</code> with a given <code>Rationnel</code>
+	 * @param number
+	 * @return <code>Rationnel</code>
 	 */
 	public Rationnel mult(Rationnel number) {
 		return new Rationnel(this.numerateur*number.getNumerateur(), this.denominateur*number.getDenominateur());
 	}
 	
 	/**
-	 * return the real number of the division of an existing <code>Rationnel</code>
+	 * returns the real number of the division of an existing <code>Rationnel</code>
+	 * @return <code>double</code>
 	 */
 	public double toReal() {
 		return this.numerateur/this.denominateur;
