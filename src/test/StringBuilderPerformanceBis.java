a6f28da8f96707131b315ad8be2330d19a4743b6
Aurélien Cazuc <clementorus@hotmail.fr>
2017-01-26 00:52:28 +0100

    .

--- src/test/StringBuilderPerformanceBis.java
+++ src/test/StringBuilderPerformanceBis.java
@@ -1,7 +1,7 @@
 package test;
 
-public class StringBuilderPerformance {
-
+public class StringBuilderPerformanceBis {
+	
 	public static void main(String[] args) {
 		String a = "iajdiqjd";
 		String b = "dji<<w";
@@ -18,32 +18,20 @@ public class StringBuilderPerformance {
 		long selfMadeTimerConcat = 0;
 		long javaTimer = 0;
 		String tmp = null;
-		while(i < 1000000) {
-			tmp = null;
-			timer = System.nanoTime();
+		timer = System.nanoTime();
+		while(++i < 1000000) {
 			tmp = a+b+c;
-			delta = System.nanoTime()-timer;
-			System.out.println(tmp);
-			javaTimer+= delta;
-			
-			tmp = null;
-			timer = System.nanoTime();
-			tmp = new StringBuilder().append(a).append(b).append(c).toString();
-			delta = System.nanoTime()-timer;
-			System.out.println(tmp);
-			selfMadeTimer+= delta;
-
-			tmp = null;
-			timer = System.nanoTime();
-			tmp = a.concat(b).concat(c);
-			delta = System.nanoTime()-timer;
-			System.out.println(tmp);
-			selfMadeTimerConcat+= delta;
-			i++;
 		}
+		javaTimer+= System.nanoTime()-timer;
+		i = 0;
+		timer = System.nanoTime();
+		while(++i < 1000000) {
+			tmp = new StringBuilder().append(a).append(b).append(c).toString();
+		}
+		selfMadeTimer+= System.nanoTime()-timer;
 		System.out.println(tmp);
 		System.out.println("Self made timer total time : "+selfMadeTimer+", average timer : "+(selfMadeTimer/i)); 
-		System.out.println("Self made concat timer total time : "+selfMadeTimerConcat+", average timer : "+(selfMadeTimerConcat/i));
+		//System.out.println("Self made concat timer total time : "+selfMadeTimerConcat+", average timer : "+(selfMadeTimerConcat/i));
 		System.out.println("Java timer total time : "+javaTimer+", average timer : "+(javaTimer/i)); 
 	}
 }
