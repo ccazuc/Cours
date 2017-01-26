618a1163f075d802c010525a84a35a02991838b7
Aurélien Cazuc <clementorus@hotmail.fr>
2017-01-26 01:06:32 +0100

    .

--- src/test/StringBuilderPerformanceBis.java
+++ src/test/StringBuilderPerformanceBis.java
@@ -12,26 +12,34 @@ public class StringBuilderPerformanceBis {
 			i++;
 		}
 		i = 0;
-		long timer;
-		long delta;
-		long selfMadeTimer = 0;
-		long selfMadeTimerConcat = 0;
-		long javaTimer = 0;
-		String tmp = null;
-		timer = System.nanoTime();
-		while(++i < 1000000) {
-			tmp = a+b+c;
+		int j = 0;
+		while(j < 15) {
+			long timer;
+			long delta;
+			long selfMadeTimer = 0;
+			long selfMadeTimerConcat = 0;
+			long javaTimer = 0;
+			final int ITERATION = 10000;
+			String tmp = null;
+			i = 0;
+			timer = System.nanoTime();
+			while(++i < ITERATION) {
+				tmp+= new StringBuilder().append(a).append(b).append(c).toString();
+			}
+			selfMadeTimer+= System.nanoTime()-timer;
+			i = 0;
+			tmp = null;
+			timer = System.nanoTime();
+			while(++i < ITERATION) {
+				tmp+= a+b+c;
+			}
+			javaTimer+= System.nanoTime()-timer;
+			//System.out.println(tmp);
+			System.out.println("Self made timer total time : "+selfMadeTimer+", average timer : "+(selfMadeTimer/ITERATION)); 
+			//System.out.println("Self made concat timer total time : "+selfMadeTimerConcat+", average timer : "+(selfMadeTimerConcat/i));
+			System.out.println("Java timer total time : "+javaTimer+", average timer : "+(javaTimer/ITERATION)); 
+			
+		j++;
 		}
-		javaTimer+= System.nanoTime()-timer;
-		i = 0;
-		timer = System.nanoTime();
-		while(++i < 1000000) {
-			tmp = new StringBuilder().append(a).append(b).append(c).toString();
-		}
-		selfMadeTimer+= System.nanoTime()-timer;
-		System.out.println(tmp);
-		System.out.println("Self made timer total time : "+selfMadeTimer+", average timer : "+(selfMadeTimer/i)); 
-		//System.out.println("Self made concat timer total time : "+selfMadeTimerConcat+", average timer : "+(selfMadeTimerConcat/i));
-		System.out.println("Java timer total time : "+javaTimer+", average timer : "+(javaTimer/i)); 
 	}
 }
